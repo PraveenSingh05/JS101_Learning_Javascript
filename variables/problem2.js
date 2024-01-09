a9719d68fadc128e6df700b415c013a0c2ee2117
PraveenSingh05 <05praveensingh.ps@gmail.com>
2024-01-09 18:33:33 +0530

    variable assignment added

---
+++ variables/problem2.js
@@ -0,0 +1,6 @@
+let name = "Praveen Singh";
+console.log(name);
+name = "Vinay Singh";
+console.log(name);
+name = "Poonam Singh";
+console.log(name);
---
+++ variables/problem3.js
@@ -0,0 +1,4 @@
+let name = "Praveen Singh";
+let age = 23;
+console.log(name,age);
+console.log(typeof (name),typeof (age));
